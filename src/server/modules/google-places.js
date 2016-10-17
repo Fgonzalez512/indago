d367c7e02e37a8ca079a09f4eba52d8f3817282c
Christopher Katzmann <christopherkatzmann@Christophers-MacBook-Pro.local>
2016-10-17 15:41:44 -0500

    build places module

--- src/server/modules/google-places.js
+++ src/server/modules/google-places.js
@@ -1,7 +1,19 @@
 const request = require('request');
+const base = 'https://maps.googleapis.com/maps/api/place/';
 
-request('https://maps.googleapis.com/maps/api/place/radarsearch/json?location=51.503186,-0.126446&radius=5000&type=museum&key=AIzaSyBzSrtzkd0500etRD3IsxjsKSON9ipJLBo', function (error, response, body) {
-  if (!error && response.statusCode == 200) {
-    console.log(body); // Show the HTML for the Google homepage.
-  }
-});
+module.exports = {
+  radarSearch : (lat, long, type) => {
+    return request(base + 'radarsearch/json?location=' + lat + ',' + long + '&radius=5000&type=' + type + '&key=AIzaSyBzSrtzkd0500etRD3IsxjsKSON9ipJLBo', (error, response, body) => {
+      if (!error && response.statusCode == 200) {
+        return body;
+      }
+    });
+  },
+  details : (placeID) => {
+    return request(base + 'details/json?placeid=' + placeID + '&key=AIzaSyBzSrtzkd0500etRD3IsxjsKSON9ipJLBo', (error, response, body) => {
+      if(!error && response.statusCode == 200) {
+        return body;
+      }
+    });
+  },
+};
