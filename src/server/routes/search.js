535b90646f687075c18265894ab7f191ec31244b
Christopher Katzmann <christopherkatzmann@Christophers-MacBook-Pro.local>
2016-10-18 11:51:35 -0500

    search populates page with results

--- src/server/routes/search.js
+++ src/server/routes/search.js
@@ -2,14 +2,23 @@ const express = require('express');
 const router = express.Router();
 const googlePlaces = require('../../../src/server/modules/google-places.js');
 const radarSearch = googlePlaces.radarSearch;
+const nearbySearch = googlePlaces.nearbySearch;
 const placeDetails = googlePlaces.details;
 
 router.get('/', function (req, res, next) {
-  res.render('pages/search');
+  res.render('pages/search', {
+    results : [],
+  });
 });
 
 router.post('/', function(req, res, next) {
-  console.log(req.body);
+  var lat = req.body.location.split(',')[0];
+  var long = req.body.location.split(',')[1];
+  nearbySearch(lat, long, req.body.keyword, (data) => {
+    res.render('pages/search', {
+      results : data.results,
+    });
+  });
 });
 
 module.exports = router;
