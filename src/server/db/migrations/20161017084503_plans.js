efccf7cb1b114d3da5f1f093ea7aad5f71046976
Diego Goding <diego.goding@gmail.com>
2016-10-17 22:55:48 -0500

    knex migrations

--- src/server/db/migrations/20161017084503_plans.js
+++ src/server/db/migrations/20161017084503_plans.js
@@ -1,26 +1,24 @@
 
 exports.up = function(knex, Promise) {
-  return knex.schema.createTableIfNotExists('places',(table)=>{
+  return knex.schema.createTableIfNotExists('plans',(table)=>{
     table.increments();
-    table.integer('plan_id').defaultTo(1).notNullable();
     table.integer('plan_type_id').defaultTo(1).notNullable();
+    table.date('date');
     table.boolean('is_favorite').defaultTo(false).notNullable();
+    table.integer('score').defaultTo(0).notNullable();
     table.string('name').defaultTo('').notNullable();
     table.string('address').defaultTo('').notNullable();
     table.string('city').defaultTo('').notNullable();
     table.string('state',2).defaultTo('').notNullable();
     table.string('zipcode',5).defaultTo('').notNullable();
-    table.time('start_time');
-    table.time('end_time');
     table.integer('coordinate_x');
     table.integer('coordinate_y');
     table.integer('version').defaultTo(1).notNullable();
-    table.integer('plan_version').defaultTo(1).notNullable();
     table.boolean('is_enable').defaultTo(true).notNullable();
     table.timestamps(true,true);
   });
 };
 
 exports.down = function(knex, Promise) {
-  return knex.schema.dropTableIfExists('places');
+  return knex.schema.dropTableIfExists('plans');
 };
