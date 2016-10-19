3e4f8fa2f71fcbb4c2f735524c9f159479fc96db
Frank Gonzalez <frank.gonzalez628@gmail.com>
2016-10-19 16:45:36 -0500

    merge

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -4,16 +4,9 @@ const knex = require('../db/connection.js');
 
 
 router.get('/', function(req, res, next) {
-  if (req.session.loggedIn) {
-    var userID = Number.parseInt(req.session.user.id);
-    knex('users').where('id', '=', userID).then(function(info) {
-      res.render('index', {
-        info: info
-      });
-    });
-  } else {
-    res.render('index');
-  }
+
+  res.render('index');
+
 });
 
 router.get('/myplan', function(req, res, next) {
