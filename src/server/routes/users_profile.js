1399c2589815bb08b97c954190a776c0692a5064
Samuel Rosenbluth <samuelrosenbluth@Samuels-MacBook-Air.local>
2016-10-19 11:30:21 -0500

    merge sam

--- src/server/routes/users_profile.js
+++ src/server/routes/users_profile.js
@@ -11,13 +11,12 @@ const knex = require('knex');
 
 router.get('/:user_id/', function (req, res, next) {
   res.render('pages/user_edit');
+  //users/profile/user_id;
+  var user_id = req.params.user_id;
+  User.all().where('id', user_id).first().then((user)=>{
 
-  // var user_id = req.params.user_id;
-  // User.all().where('id', user_id).first().then((user)=>{
-  //   // res.render('profile',{user: user});
-  //   console.log(user);
-  //
-  // });
+    res.render('profile',{user_id : user_id});
+  });
 });
 
 
@@ -26,12 +25,12 @@ router.get('/:user_id/', function (req, res, next) {
   //get user information from the database
 
   //send user infromation to the render page
-// router.patch('/:user_id/', function (req, res, next) {
-//   var current_user_profile = req.body;
-//   knex('users_profile').where({user: req.body.user}).update({user: req.body.username, first_name: req.body.first_name, last_name: req.body.user.last_name, username: req.body.username, email: req.body.user.email}).then(function(){
-//     res.render('./pages/user_edit');
-//   });
-// });
+router.put('/:user_id/', function (req, res, next) {
+  var user_id = req.body.user_id;
+  knex('users_profile').where({user: req.body.user}).update({user: req.body.username, first_name: req.body.first_name, last_name: req.body.user.last_name, username: req.body.username, email: req.body.user.email}).then(function(){
+    res.render('./pages/user_edit');
+  });
+});
 
 
 //
@@ -53,6 +52,35 @@ router.get('/:user_id/', function (req, res, next) {
   //get updated information from req.body
 
   //update user information in the database
+
+
+router.delete('/:user_id', (req, res, next)=>{
+});
+
+
+router.delete('/:user_id', (req, res, next) => {
+  let user_id;
+
+  knex('user')
+    .where('id', req.params.id)
+    .first()
+    .then((user_id) => {
+      if (!user_id) {
+        return next();
+      }
+
+      return knex('users')
+        .del()
+        .where('id', req.params.id);
+    })
+    .then(() => {
+      delete user_id.id;
+      res.send(user);
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
 
 
 module.exports = router;
