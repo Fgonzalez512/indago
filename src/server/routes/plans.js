9c3fbb672e8ff8e5b91bbb35f252639308dc077d
Roberto  Varela <rvarela11@Roberts-MacBook-Pro.local>
2016-10-19 10:53:50 -0500

    plans page

--- src/server/routes/plans.js
+++ src/server/routes/plans.js
@@ -4,16 +4,21 @@ const bcrypt = require('bcrypt-nodejs');
 const methodOverride = require('method-override');
 const knex = require('../db/connection.js');
 
-// router.get('/', function(req, res) {
-//   res.render('pages/plans');
-// });
+router.get('/', function(req, res) {
+  res.render('pages/plans');
+});
 
 router.get('/cities/:city', function(req, res, next) {
   let cityID = req.params.city;
-  knex('plans').where('city', cityID).first().then((city) => {
+
+  res.locals.page_type = cityID;
+
+  console.log(res.locals.page_type);
+
+  knex('plans').where('city', cityID).then((plans) => {
+    console.log(plans);
     res.render('pages/plans', {
-      city: city,
-      name: city,
+      plans: plans
     });
   });
 });
